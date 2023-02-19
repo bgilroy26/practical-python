0f987934b9905bfb62bbc10d41bb6fca3ef7fa3a
Brendan Gilroy <bdgilroy@gmail.com>
2023-02-19 17:31:00 -0500

    introduction -- numbers

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+starting_height = 100
+height = starting_height
+bounce_proportion = 0.6
+bounce_count = 0
+
+while height > 0.00005:
+    print(bounce_count, height)
+    height = height * bounce_proportion
+    bounce_count = bounce_count + 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,37 @@
 # mortgage.py
 #
 # Exercise 1.7
+extra_payment_start_month = int(input('extra_payment_start_month: '))
+extra_payment_end_month = int(input('extra_payment_end_month: '))
+extra_payment = int(input('extra_payment: '))
+extra_pmt_months = range(extra_payment_start_month, extra_payment_end_month+1)
+
+principal = 500000.0
+rate = 0.05
+payment = 0.0
+regular_payment = 2684.11
+extra_payment = regular_payment + extra_payment
+total_paid = 0.0
+months = 0
+
+
+while principal > 0:
+
+    months = months + 1
+
+    if months in extra_pmt_months:
+        payment = extra_payment
+    else:
+        payment = regular_payment
+
+    if principal = principal * (1+rate/12) - payment <= 0:
+        principal = 0.0
+        total_paid = total_paid + (payment
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+
+    print(f'{months}, {total_paid:.2f}, {principal:.2f}')
+
+print(f'Total paid {total_paid:.2f}')
+print(f'Number of months {months}')
