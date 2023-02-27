999207e438de97a2331f20809c1ecf6b6dbf7b8a
Brendan Gilroy <bdgilroy@gmail.com>
2023-02-27 12:36:11 -0500

    in sequences; updated portfolio to create dicts

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,38 +6,32 @@ import sys
 import csv
 
 
-def get_portfolio_csv_lines(file_name):
-    'open csv containing portfolio data: symbols, prices, num_shares'
+def calculate_cost(file_name):
+    'calculate cost of a portfolio based on csv input of symbols, prices, num_shares'
+    portfolio_cost = 0.0
+    portfolio = []
+
     with open(file_name) as f:
         rows = csv.reader(f)
         headers = next(rows)
-        return list(rows)
 
+        for row in rows:
+            stock = dict(zip(headers, row))
+            portfolio.append(stock)
 
-def calculate_cost(lines):
-    'calculate cost of a portfolio based on csv input of symbols, prices, num_shares'
-    portfolio_cost = 0.0
+        for idx, stock in enumerate(portfolio):
+            try:
+                num_shares = int(stock['shares'])
+                price = float(stock['price'])
+            except ValueError:
+                print('Row ' + idx + ': Could not parse', line)
+                continue
 
-    for line in lines:
-        _, num_shares, price = line
-        try:
-            num_shares = int(num_shares)
-        except ValueError:
-            print('Could not parse', line)
-            continue
-        try:
-            price = float(price.strip())
-        except ValueError:
-            print('Could not parse', line)
-            continue
-
-        portfolio_cost = portfolio_cost + (num_shares * price)
+            portfolio_cost = portfolio_cost + (num_shares * price)
 
     return portfolio_cost
 
 
 file_name = sys.argv[1]
 
-lines = get_portfolio_csv_lines(file_name)
-
-print('Total cost:', calculate_cost(lines))
+print('Total cost:', calculate_cost(file_name))
