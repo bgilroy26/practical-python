dc488b6d51ab8a3d829505236966d898d964ff7f
Brendan Gilroy <bdgilroy@gmail.com>
2023-03-06 20:16:12 -0500

    complete up to 3.14, lots of digging around

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,28 +4,80 @@
 import csv
 
 
-def parse_csv(filename, select=[]):
+def parse_csv(filename, select=[], types=[], has_headers=True, delimiter=',', silence_errors=False):
     '''
     Parse a csv into a list of records
     '''
-    with open(filename) as f:
-        rows = csv.reader(f)
 
-        # read the file headers
-        headers = next(rows)
+    if select and not has_headers:
+        raise RuntimeError('"select" argument requires column headers')
+
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
         records = []
 
-        if select == []:
-            for row in rows:
+        # read the file headers
+        if has_headers:
+            headers = next(rows)
+
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                names = [headers[index] for index in indices]
+
+                for idx, row in enumerate(rows):
+                    if not row:
+                        continue
+
+                    if types:
+                        try:
+                            record = { name: type(row[index]) for name, index, type in
+                                    zip(names, indices, types) }
+                        except ValueError as e:
+                            if silence_errors:
+                                continue
+                            print('Row', str(idx) + ': Could not convert')
+                            print('Row', str(idx) + ':', e)
+                            continue
+                    else:
+                        record = { name: row[index] for name, index in
+                                zip(names, indices) }
+                    records.append(record)
+            else:
+                for idx, row in enumerate(rows):
+                    if not row:
+                        continue
+
+                    if types:
+                        record = {}
+                        for header, type, item in zip(headers, types, row):
+                            try:
+                                record[header] = type(item)
+                            except ValueError as e:
+                                if silence_errors:
+                                    continue
+                                print('Row', str(idx) + ': Could not convert')
+                                print('Row', str(idx) + ':', e)
+                                continue
+                    else:
+                        record = dict(zip(headers, row))
+                    records.append(record)
+
+        else:
+            for idx, row in enumerate(rows):
                 if not row:
                     continue
-                record = dict(zip(headers, row))
-                records.append(record)
-        else:
-            indices = [headers.index(colname) for colname in select]
-            names = [headers[index] for index in indices]
-            for row in rows:
-                record = { name: row[index] for name, index in zip(names, indices) }
+
+                if types:
+                    try:
+                        record = tuple(type(item) for type, item in zip(types, row))
+                    except ValueError as e:
+                        if silence_errors:
+                            continue
+                        print('Row', str(idx) + ': Could not convert')
+                        print('Row', str(idx) + ':', e)
+                        continue
+                else:
+                    record = tuple(row)
                 records.append(record)
 
     return records
--- Work/pcost.py
+++ Work/pcost.py
@@ -3,31 +3,18 @@
 # Exercise 1.27
 
 import sys
-import csv
-
+import report
 
 def calculate_cost(file_name):
     'calculate cost of a portfolio based on csv input of symbols, prices, num_shares'
     portfolio_cost = 0.0
-    portfolio = []
 
-    with open(file_name) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
+    portfolio = report.read_portfolio(file_name)
 
-        for row in rows:
-            stock = dict(zip(headers, row))
-            portfolio.append(stock)
-
-        for idx, stock in enumerate(portfolio):
-            try:
-                num_shares = int(stock['shares'])
-                price = float(stock['price'])
-            except ValueError:
-                print('Row ' + idx + ': Could not parse', line)
-                continue
-
-            portfolio_cost = portfolio_cost + (num_shares * price)
+    for idx, stock in enumerate(portfolio):
+        num_shares = int(stock['shares'])
+        price = float(stock['initial_price'])
+        portfolio_cost = portfolio_cost + (num_shares * price)
 
     return portfolio_cost
 
