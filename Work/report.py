663889e5136a8d215f43e721fed129642a90c43c
Brendan Gilroy <bdgilroy@gmail.com>
2023-03-07 18:57:38 -0500

    cleaned out some debugging stuff

--- Work/report.py
+++ Work/report.py
@@ -4,15 +4,10 @@ import fileparse
 
 def read_portfolio(file_name):
     'open csv containing portfolio data: symbols, prices, shares'
-    try:
-        portfolio = fileparse.parse_csv(file_name,
-                                        select=['name', 'shares', 'price'],
-                                        types=[str, int, float]
-                                        )
-    except TypeError as e:
-        if 'TextIOWrapper' in e:
-            print('inside')
-            return
+    portfolio = fileparse.parse_csv(file_name,
+                                    select=['name', 'shares', 'price'],
+                                    types=[str, int, float]
+                                    )
     return portfolio
 
 
